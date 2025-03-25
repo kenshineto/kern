9b26a288ae243b9497316e7c94dbc7914a09b14f
Freya Murphy <freya@freyacat.org>
2025-03-25 17:42:33 -0400

    remove legacy bullshit

--- vmtables.d
+++
@@ -1,5 +0,0 @@
-vmtables.o: kernel/vmtables.c include/common.h include/types.h \
- include/params.h include/defs.h include/kdefs.h include/debug.h \
- include/cio.h include/support.h include/lib.h include/klib.h \
- include/x86/ops.h include/kmem.h include/procs.h include/vm.h \
- include/x86/arch.h
