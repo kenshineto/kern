9116173d53e9ebc2388131fe8e5fc7e0f18c40c5
Freya Murphy <freya@freyacat.org>
2025-04-15 22:49:26 -0400

    move error.h out of symlink

--- kernel/include/comus/error.h
+++ kernel/include/comus/error.h
@@ -1,1 +1,35 @@
-../../../user/include/error.h
+/**
+ * @file errno.h
+ *
+ * @author Freya Murphy <freya@freyacat.org>
+ *
+ * Error codes.
+ */
+
+#ifndef ERROR_H_
+#define ERROR_H_
+
+// public error codes
+#define SUCCESS (0)
+#define E_SUCCESS SUCCESS
+#define E_FAILURE (-1)
+#define E_BAD_PARAM (-2)
+#define E_BAD_FD (-3)
+#define E_NO_CHILDREN (-4)
+#define E_NO_MEMORY (-5)
+#define E_NOT_FOUND (-6)
+#define E_NO_PROCS (-7)
+
+// kernel error codes
+#define E_EMPTY_QUEUE (-100)
+#define E_NO_PCBS (-101)
+#define E_NO_PTE (-102)
+#define E_LOAD_LIMIT (-103)
+
+// exit status values
+#define EXIT_SUCCESS (0)
+#define EXIT_FAILURE (-1)
+#define EXIT_KILLED (-101)
+#define EXIT_BAD_SYSCALL (-102)
+
+#endif /* error.h */
