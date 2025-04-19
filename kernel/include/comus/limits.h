6f37ebc6e5080c865fd8b9f20ee37e44918ed61c
Freya Murphy <freya@freyacat.org>
2025-04-18 21:15:14 -0400

    more pci devices

--- kernel/include/comus/limits.h
+++ kernel/include/comus/limits.h
@@ -9,6 +9,9 @@
 /// max number of processes
 #define N_PROCS 256
 
+/// max numer of pci devices
+#define N_PCI_DEV 256
+
 /// length of terminal buffer
 #define TERM_MAX_WIDTH 480
 #define TERM_MAX_HEIGHT 270
