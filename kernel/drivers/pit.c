50447355b0a0c6d390010c3d209de5e9da2f012f
Freya Murphy <freya@freyacat.org>
2025-05-06 13:21:44 -0400

    set pit into mode3 (fixes uefi)

--- kernel/drivers/pit.c
+++ kernel/drivers/pit.c
@@ -15,7 +15,7 @@ uint32_t pit_read_freq(uint8_t chan)
 {
 	uint16_t div = 0;
 	cli();
-	outb(CMD, 0); // clear bits
+	outb(CMD, 0x36); // clear bits
 	div = inb(chan); // low byte
 	div |= inb(chan) << 8; // highbyte
 	sti();
@@ -26,7 +26,7 @@ void pit_set_freq(uint8_t chan, uint32_t hz)
 {
 	uint16_t div = BASE / hz;
 	cli();
-	outb(CMD, 0xb6);
+	outb(CMD, 0x36);
 	outb(chan, div & 0xFF); // low byte
 	outb(chan, (div & 0xFF00) >> 8); // high byte
 	sti();
